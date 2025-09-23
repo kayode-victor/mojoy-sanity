fc7ce2fbb654005a74fbfe050a3467a4f77c0958
Bankole Akorede.O <koredebankole2000@gmail.com>
2025-09-23 13:58:08 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-      },
-    ],
-    domains: ["lh3.googleusercontent.com"], // Add the hostname here
-  },
+  remotePatterns: [
+    {
       protocol: "https",
       hostname: "cdn.sanity.io",
       pathname: "**",
